08b9f7ab7bad7a54defb63f4f3c8ee51b16420f0
jimen8930 <128547615+jimen8930@users.noreply.github.com>
2023-04-23 22:04:10 -0400

    Added a button element to css, btn class, and btn grid class.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //I need questions ex: var questions =
 //I need a score box
 //I need a timer on the left of the screen
-//I need 
+//I need a function to start the quiz.
+//I need a timer at the top of the screen maybe to the left.
+//I need the questions to continue to pop  up after the question has been answered.
+//My answer choices need a function.
+//I need an event listener for method for when the buttons are clicked. 
