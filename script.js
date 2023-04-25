3c8fe83552243b06cce8a9783693bf9bc4257d9b
Elizabeth Jimenez <2good2btru@Elizabeths-MacBook-Air-3.local>
2023-04-25 16:38:56 -0400

    Added some functions and a for loop.

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-//I need questions ex: var questions =
+//I need varible for my questions ex: var questions =
 //I need a score box
 //I need a timer on the left of the screen
 //I need a function to start the quiz.
+function startQuiz() {
+
+}
+//I need a function for the question.
+function nextQuestion () {
+
+}
 //I need a timer at the top of the screen maybe to the left.
-//I need the questions to continue to pop  up after the question has been answered.
+//I need the questions to continue to pop up after the question has been answered. May be for loop.
+for (let i = 0; i < questions.length; i++) {
+    nextQuestion += questions[i] + "<br>";
+  }
 //My answer choices need a function.
-//I need an event listener for method for when the buttons are clicked. 
+function answerChoice() {
+
+}
+//I need an event listener for method for when the buttons that are clicked. 
+
+//I need an array for the questions. 
